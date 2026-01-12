b03c96420c16967e83135c5a50f58bbcb50ebad4
Matthew Dalrymple <matt.dalrymple@gmail.com>
2026-01-11 23:37:42 -0800

    Setup works without Run

--- setup.bat
+++ setup.bat
@@ -1,6 +1,9 @@
 set STORAGE_ROOT=../storage
 
 pushd orchis
+docker compose build
+docker compose up -d
 docker compose exec web python manage.py makemigrations
 docker compose exec web python manage.py migrate
+docker compose down
 popd
